4f731e3d9aadfb5cb55707fa88cb260ffeef01c6
SalvaZum <szumstein248@cesd.edu.ar>
2025-10-15 21:30:35 -0300

    cambios al arbol

--- src/parcial/Arbolbinario.java
+++ src/parcial/Arbolbinario.java
@@ -54,12 +54,34 @@ public class Arbolbinario {
 			}
 		}
 	}
+	public static int contarHojas(Nodo nodo) {
+		if (nodo == null) { //caso base
+			return 0;
+		}
+		if (nodo .izquierdo == null && nodo.derecho == null) {
+			return 1;
+		}
+		return contarHojas(nodo.izquierdo) + contarHojas(nodo.derecho);
+	}
+	public static int contarRamas(Nodo nodo) {
+		if (nodo == null || (nodo.izquierdo == null && nodo.derecho == null)) return 0;
+		return 1 + contarRamas(nodo.izquierdo) + contarRamas(nodo.derecho);
+	}
+	public static int altura(Nodo nodo) {
+		if (nodo == null) {
+			return 0;
+		}
+		int altizq = altura(nodo.izquierdo);
+		int altder = altura(nodo.derecho);
+		if (altder < altizq) return 1 + altizq;
+		else return 1 + altder;
+	}
 	
 	
 	public static void main(String[] args) {
 			Arbolbinario main = new Arbolbinario();
 			ArbolbinarioOrdenado arbol = main.new ArbolbinarioOrdenado();
-			int[] dato = {400,100,700,50,200,1200,15,75,300};
+			int[] dato = {400,100,700,50,200,1200,15,75,300,28,983,600};
 			for (int i = 0; i < dato.length; i++) {
 				arbol.insertar(dato[i]);
 			}
@@ -72,6 +94,11 @@ public class Arbolbinario {
 			
 			System.out.println("\npostorden: ");
 			arbol.postorden(arbol.raiz);
+			
+			System.out.println("\nCantidad de hojas: " + contarHojas(arbol.raiz));
+			System.out.println("\nCantidad de Ramas: " + contarRamas(arbol.raiz));
+			System.out.println("\nAltura: " + altura(arbol.raiz));
+			
 	}
 
 }
